2799f5167da6e83dcf901cbc17a1747ce943ec51
madebyfabian <beer.fabian@mail.de>
2020-10-11 19:30:19 +0200

    Changed from plugin to component

---
+++ src/FocusVisible.vue
@@ -0,0 +1,82 @@
+<template>
+  <div
+    v-bind:is="elType"
+    :focus-visible="focusVisible || null"
+    @mousedown="handleMousedown"
+    @compositionstart="handleCompositionstart"
+    @touchstart="handleTouchstart">
+
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FocusVisible',
+
+    props: {
+      elType: {
+        type: String,
+        default: 'div'
+      }
+    },
+
+    data: () => ({
+      focusVisible: true
+    }),
+
+    methods: {
+      handleCompositionstart() {
+        this.focusVisible = true
+      },
+
+      handleKeydown() {
+        this.focusVisible = true
+      },
+
+      handleFocusin() {
+        this.focusVisible = true
+      },
+
+      handleTouchstart() {
+        this.focusVisible = false
+      },
+
+      handleMousedown(e) {
+        const target = e.target,
+              targetTagName = target.tagName.toLowerCase()
+
+        if (target.isContentEditable || targetTagName === 'textarea')
+          return this.focusVisible = true
+
+        if (targetTagName === 'input') 
+          return this.focusVisible = ![ 
+            'radio', 
+            'checkbox', 
+            'range', 
+            'button', 
+            'file', 
+            'reset', 
+            'submit'
+          ].includes(target.type)
+
+        return this.focusVisible = false
+      }
+    },
+
+    mounted() {
+      window.addEventListener('keydown', this.handleKeydown)
+    }
+  }
+</script>
+
+<style lang="scss">
+  *:focus {
+    box-shadow: none!important;
+    outline: none!important;
+  }
+
+  [focus-visible] :focus {
+    box-shadow: 0 0 0 2px #0498FB!important;
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { VueFocusVisible } from './VueFocusVisible'
 
 Vue.config.productionTip = false
-
-Vue.use(VueFocusVisible)
 
 new Vue({
   render: h => h(App),
